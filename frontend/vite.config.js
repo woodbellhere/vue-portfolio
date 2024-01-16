de83d81cce531fc908410c23eef2c1934a117200
WoodBell wong <wangjd28@mail3.sysu.edu.cn>
2024-01-16 20:32:21 +0800

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import compression from "./node_modules/vite-plugin-compression2/dist/index";
+import imagemin from "unplugin-imagemin/vite";
+import { chunkSplitPlugin } from "./node_modules/vite-plugin-chunk-split/dist/index";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  build: {
+    assetsInlineLimit: 8 * 1024,
+  },
+  plugins: [
+    vue(),
+    compression(),
+    imagemin(),
+    // 这个插件已经有报错了
+    chunkSplitPlugin({
+      customSplitting: {
+        "vue-system": ["vue", "vue-router", "vuex"],
+        " utils": ["./src/utils"],
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/api": "http://localhost:1337",
